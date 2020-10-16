e24fdddb4b7145854bfe92e674115ce0014de206
harihp2000 <54673499+harihp2000@users.noreply.github.com>
2020-10-16 18:41:02 +0530

    final-commit

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Quotes.css';
 import TwitterIcon from '../assets/twitter.svg';
 
 function Quotes() {
+
+    const [quote, setQuote] = useState('');
+    const [author, setAuthor] = useState('');
+    
+    useEffect(() => {
+        getQuote()
+    }, []);
+
+    const getQuote = () => {
+        let url=`https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`;
+        fetch(url) 
+            .then(res => res.json()) 
+            .then(data => {
+                let dataQuotes = data.quotes;
+                let randomNum = Math.floor(Math.random()* dataQuotes.length);
+                let randomQuote = dataQuotes[randomNum];
+                setQuote(randomQuote.quote);
+                setAuthor(randomQuote.author);
+            })
+    }
+
+    const handleClick = () => {
+        getQuote();
+    }
     return (
         <div className="quotes">
             <div className="quotes__text">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus accumsan tempus. Proin a sem dolor. Donec vitae sollicitudin metus. Donec sapien enim, semper ut arcu quis, porta imperdiet libero.
-                </p>
+                <p>{quote}</p>
             </div>
 
             <div className="quotes__author">
-                <p>Lorem Ipsum</p>
+                <p>- {author}</p>
             </div>
 
             <div className="quotes__buttons">
@@ -21,7 +43,7 @@ function Quotes() {
                         <span><img src={TwitterIcon} alt="" /></span>
                     </a>
                 </div>
-                <button className="new_quote">New Quote</button>
+                <button onClick= {handleClick}className="new_quote">New Quote</button>
             </div>
 
             
